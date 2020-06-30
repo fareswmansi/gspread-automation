ed58f90c28334940c633e427245e234a2cd86d16
Fares Mansi <faresmansi@Faress-MacBook-Pro.local>
2020-06-30 12:54:36 +0300

    ninth commit

--- code.py
+++ code.py
@@ -3,37 +3,28 @@ from functions import number_loop, area_loop, building_loop, location_loop, numb
 from variables import scope, credentials, worksheet, client
 
 
-
-
 print("Welcome to the google sheet automation program, to begin automating, press YES")
 first_choice = raw_input('')
 
 if first_choice == 'yes' or first_choice == 'YES':
     print("What is the name of the sheet you would like to access?")
     sheet_name = raw_input('')
-
     worksheet = client.open(sheet_name).sheet1
 
+    #split database loop and allocate each type to a certain list
     number_loop(database_list_of_lists, number_list)
-
     area_loop(database_list_of_lists, area_list)
-
     building_loop(database_list_of_lists, building_list)
-
     location_loop(database_list_of_lists, location_list)
 
+    #create coordinates for input in google sheet
     number_coordinate_create(number_coordinate_list, number_list)
-
     area_coordinate_create(area_coordinate_list, area_list)
-
     building_coordinate_create(building_coordinate_list, building_list)
-
     location_coordinate_create(location_coordinate_list, location_list)
 
+    #input data from seperate lists into google sheet using coordinates
     number_input(number_list, number_coordinate_list)
-
     area_input(area_list, area_coordinate_list)
-
     building_input(building_list, building_coordinate_list)
-
     location_input(location_list, location_coordinate_list)
