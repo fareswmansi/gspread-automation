e86e7bcb764c066895e3ff07dc285bd74118a8d1
Fares Mansi <faresmansi@Faress-MacBook-Pro.local>
2020-07-08 14:09:50 +0300

    thirty second commit

--- code.py
+++ code.py
@@ -1,36 +1,33 @@
-from lists import database_list_of_lists, number_list, area_list, building_list, location_list, test_coordinate_list, row_coordinate, recurring_customers, recurring_customers_raw, final_recurring_customers, test_list, test_test_list, first_check_list, final_check_list, write_in_me, name_list, second_test_coordinate_list, second_row_coordinate
-from functions import number_loop, area_loop, building_loop, location_loop, find_strings, row_coordinate_seperate, area_input, building_input, location_input, find_customers, customer_seperate, find_recurring, getting_cell_value, matched_string, find_matched_string, first_check, you_may_pass, progress_bar, name_loop, row_coordinate_seperate_failedInput, area_input_failedInput, building_input_failedInput, location_input_failedInput
+from functions import progress_bar, first_check, you_may_pass, name_loop, number_loop, area_loop, building_loop, location_loop, find_strings, row_coordinate_seperate, row_coordinate_seperate_failedInput, area_input, second_area_input, create_index, second_area_loop, second_building_loop, second_location_loop, second_building_input, second_location_input,  building_input, location_input
+from lists import test_coordinate_list, number_list, row_coordinate, name_list, building_list, location_list, area_list, database_list_of_lists, final_check_list, first_check_list, second_test_coordinate_list, row_coordinate, second_row_coordinate, test_test_test_test, use_me, index_list, second_area_list, second_building_list, second_location_list
 
-raw_input("Welcome to the gspread automation program. To continue, press enter.")
-
+#check spreadsheet compatibility
 first_check(first_check_list, final_check_list)
 you_may_pass(final_check_list)
 
 progress_bar()
-#split data from databse and input into list in correct order
+#create lists for data input using number
 name_loop(database_list_of_lists, name_list)
 number_loop(database_list_of_lists, number_list)
 area_loop(database_list_of_lists, area_list)
 building_loop(database_list_of_lists, building_list)
 location_loop(database_list_of_lists, location_list)
 
-#create coordinates for data input 
-find_strings(test_coordinate_list, number_list, name_list, second_test_coordinate_list)
+#create coordinates using number and name
+find_strings(test_coordinate_list, number_list, name_list, second_test_coordinate_list, use_me)
 row_coordinate_seperate(test_coordinate_list, number_list, row_coordinate)
-row_coordinate_seperate_failedInput(second_test_coordinate_list, name_list, second_row_coordinate)
+row_coordinate_seperate_failedInput(second_test_coordinate_list, test_test_test_test, name_list, second_row_coordinate)
+create_index(use_me, number_list, index_list)
 
-#input data into google sheet
+#create lists for data input using name 
+second_area_loop(index_list, area_list, second_area_list)
+second_building_loop(index_list, building_list, second_building_list)
+second_location_loop(index_list, location_list, second_location_list)
+
+#input data
 area_input(row_coordinate, area_list, number_list)
-area_input_failedInput(second_row_coordinate, area_list, name_list)
-building_input(row_coordinate, building_list, number_list)
-building_input_failedInput(second_row_coordinate, building_list, name_list)
-location_input(row_coordinate, location_list, number_list)
-location_input_failedInput(second_row_coordinate, location_list, name_list)
-
-progress_bar()
-print("Recurring customers: ")
-#find recurring customers
-getting_cell_value(recurring_customers_raw, recurring_customers)
-matched_string(recurring_customers, final_recurring_customers)
-print(final_recurring_customers)
-"""find_matched_string(number_list, test_list, test_test_list)"""
+second_area_input(second_row_coordinate, second_area_list, name_list)
+building_input(row_coordinate, building_list)
+second_building_input(second_row_coordinate, second_building_list)
+location_input(row_coordinate, location_list)
+second_location_input(second_row_coordinate, second_location_list)
--- functions.py
+++ functions.py
@@ -49,141 +49,118 @@ def location_loop(database_list_of_lists, location_list):
     for i in range(len(database_list_of_lists)):
         location_list.append(database_list_of_lists[i][5])
 
-
 #find strings for number input
-def find_strings(test_coordinate_list, number_list, name_list, second_test_coordinate_list):
+def find_strings(test_coordinate_list, number_list, name_list, second_test_coordinate_list, use_me):
     for i in range(len(number_list)):
         try:
             cell = worksheet.find(number_list[i])
             test_coordinate_list.append(cell)
         except:
-            print("Number: %r not found within google spreadsheet" % (number_list[i]))
+            print("Number %r not found within the google spreadsheet" % (number_list[i]))
+            use_me.append(number_list[i])
             print("Attempting to locate input fields using name...")
             try:
                 cell = worksheet.find(name_list[i])
                 second_test_coordinate_list.append(cell)
             except:
-                print("Name: %r not found withing google spreadsheet") % (name_list[i])
+                print("Name: %r not found within the google spreadsheet." % (name_list[i]))
                 text_file = open('failed_input.txt', 'a')
-                text_file.write("Not found: " + str(number_list[i]) +  " " + str(name_list[i]) + "\n")
+                text_file.write("Not found: " + str(name_list[i]) + "\n")
                 text_file.close()
-            
+
+#create index to append data to lists to input data using name
+def create_index(use_me, number_list, index_list):
+    for i in range(len(use_me)):
+        index = number_list.index(use_me[i])
+        index_list.append(index)
+
+#append data from area list for name input
+def second_area_loop(index_list, area_list, second_area_list):
+    for i in range(len(index_list)):
+        append_me = area_list[index_list[i]]
+        second_area_list.append(append_me)
+
+#append data from building list for name input
+def second_building_loop(index_list, building_list, second_building_list):
+    for i in range(len(index_list)):
+        append_me = building_list[index_list[i]]
+        second_building_list.append(append_me)
+
+#append data from location list for name input
+def second_location_loop(index_list, location_list, second_location_list):
+    for i in range(len(index_list)):
+        append_me = location_list[index_list[i]]
+        second_location_list.append(append_me)
+
 #create coordinates for area input
 def row_coordinate_seperate(test_coordinate_list, number_list, row_coordinate):
     for i in range(len(test_coordinate_list)):
         seperateMe = str(test_coordinate_list[i]).replace('<Cell ', '')
         secondSeperate = str(seperateMe).replace('u\'%s' % (number_list[i]), '')
         thirdSeperate = str(secondSeperate).replace(' \'>', '')
-        fourthSeperate = str(thirdSeperate).replace('1', '')
-        fifthSeperate = str(fourthSeperate).replace('R', '')
-        finalSeperate = str(fifthSeperate).replace('C', '')
+        fourthSeperate = str(thirdSeperate).replace('R', '')
+        finalSeperate = str(fourthSeperate).replace('C2', '')
         row_coordinate.append(finalSeperate)
 
-#area input into google sheet, input failed entrys into empty text file
+#create coordinates for area input
+def row_coordinate_seperate_failedInput(second_test_coordinate_list, test_test_test_test, name_list, second_row_coordinate):
+    for i in range(len(second_test_coordinate_list)):
+        seperateMe = str(second_test_coordinate_list[i]).replace('<Cell ', '')
+        thirdSeperate = str(seperateMe).replace(' \'>', '')
+        fourthSeperate = str(thirdSeperate).replace('R', '')
+        finalSeperate = str(fourthSeperate).replace('C1', '')
+        sep = ' '
+        rest = finalSeperate.split(sep, 1)[0]
+        second_row_coordinate.append(rest)
+
+#input area data from number list
 def area_input(row_coordinate, area_list, number_list):
     for i in range(len(row_coordinate)):
         try:
             worksheet.update_cell(row_coordinate[i], 3, area_list[i])
         except:
-            print("Could not input area data for number: %r" % (number_list[i]))
+            print("Could not input data for number: %r" % number_list[i])
             text_file = open('failed_input.txt', 'a')
             text_file.write("Input data error: " + str(number_list[i]) + "\n")
 
-#building input into google sheet
-def building_input(row_coordinate, building_list, number_list):
+#input area data from name list
+def second_area_input(second_row_coordinate, second_area_list, name_list):
+    for i in range(len(name_list)):
+        try:
+            worksheet.update_cell(second_row_coordinate[i], 3, second_area_list[i])
+        except:
+            print("Could not input data for name: %r" % name_list[i])
+            text_file = open('failed_input.txt', 'a')
+            text_file.write("Input data error: " + str(name_list[i]) + "\n")
+
+#input building data from number list
+def building_input(row_coordinate, building_list):
     for i in range(len(row_coordinate)):
         try:
             worksheet.update_cell(row_coordinate[i], 4, building_list[i])
         except:
             pass
 
-#location innput into google sheet
-def location_input(row_coordinate, location_list, number_list):
+#input building data from name list
+def second_building_input(second_row_coordinate, second_building_list):
+    for i in range(len(second_row_coordinate)):
+        try:
+            worksheet.update_cell(second_row_coordinate[i], 4, second_building_list[i])
+        except:
+            pass
+
+#input location data from number list
+def location_input(row_coordinate, location_list):
     for i in range(len(row_coordinate)):
         try:
             worksheet.update_cell(row_coordinate[i], 5, location_list[i])
         except:
             pass
 
-#delete
-def find_customers(recurring_customers_raw):
-    values = worksheet.col_values(1)
-    recurring_customers_raw.append(values)
-#delete
-def customer_seperate(recurring_customers, recurring_customers_raw):
-    for i in range(len(recurring_customers_raw)):
-        seperatefirst = str(recurring_customers_raw[i]).replace('u\'Number\',', '')
-        seperatesecond = str(seperatefirst).replace('u\'', '')
-        finalseperate = str(seperatesecond).replace('\'', '')
-        recurring_customers.append(finalseperate)
-#delete
-def find_recurring(recurring_customers):
-    for i in range(len(recurring_customers)):
-        if recurring_customers[i] == recurring_customers[i]:
-            print(recurring_customers[i])
-        else:
-            print("No recurring customers founnd.")
-
-
-
-#gets value of all numbers in google spreadsheet
-def getting_cell_value(recurring_customers_raw, recurring_customers):
-    cell = worksheet.col_values(1, value_render_option='FORMATTED_VALUE')
-    recurring_customers_raw.append(cell)
-    for list in recurring_customers_raw:
-        list.pop(0)
-        for i in range(len(list)):
-            recurring_customers.append(list[i])
-
-#seperates 
-def matched_string(recurring_customers, final_recurring_customers):
-    for i in range(len(recurring_customers)):
-        seperatefirst = str(recurring_customers[i]).replace('u\'', '')
-        final_recurring_customers.append(seperatefirst)
-
-def find_matched_string(number_list, test_list, test_test_list):
-    for i in range(len(number_list)):
-        cell_list = worksheet.findall(number_list[i])
-        test_list.append(cell_list)
-        for i in range(len(test_list)):
-            if test_list[i][1] == True:
-                print(test_list[i][1])
-            else:
-                print("No recurring customers found.")
-
-#create coordinates for area input
-def row_coordinate_seperate_failedInput(second_test_coordinate_list, name_list, second_row_coordinate):
-    for i in range(len(second_test_coordinate_list)):
-        seperateMe = str(second_test_coordinate_list[i]).replace('<Cell ', '')
-        secondSeperate = str(seperateMe).replace('u\'%s' % (name_list[i]), '')
-        thirdSeperate = str(secondSeperate).replace(' \'>', '')
-        fourthSeperate = str(thirdSeperate).replace('1', '')
-        fifthSeperate = str(fourthSeperate).replace('R', '')
-        finalSeperate = str(fifthSeperate).replace('C', '')
-        second_row_coordinate.append(finalSeperate)
-
-#area input into google sheet, input failed entrys into empty text file
-def area_input_failedInput(second_row_coordinate, area_list, name_list):
+#input location data from name list
+def second_location_input(second_row_coordinate, second_location_list):
     for i in range(len(second_row_coordinate)):
         try:
-            worksheet.update_cell(second_row_coordinate[i], 3, area_list[i])
-        except:
-            print("Could not input area data for number: %r" % (name_list[i]))
-            text_file = open('failed_input.txt', 'a')
-            text_file.write("Input data error: " + str(name_list[i]) + "\n")
-
-#building input into google sheet
-def building_input_failedInput(second_row_coordinate, building_list, name_list):
-    for i in range(len(second_row_coordinate)):
-        try:
-            worksheet.update_cell(second_row_coordinate[i], 4, building_list[i])
-        except:
-            pass
-
-#location innput into google sheet
-def location_input_failedInput(second_row_coordinate, location_list, name_list):
-    for i in range(len(second_row_coordinate)):
-        try:
-            worksheet.update_cell(second_row_coordinate[i], 5, location_list[i])
+            worksheet.update_cell(second_row_coordinate[i], 5, second_location_list[i])
         except:
             pass
